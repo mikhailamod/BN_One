42737753bf306e3a69f231fae20872c00fe6fd16
Mikhail Amod <mickeyamod@gmail.com>
2019-09-26 22:36:31 +0200

    temp

--- dataprocess.py
+++ dataprocess.py
@@ -8,6 +8,34 @@ def total_genre(json_array, genreDict):
             for genre in tags:
                 genreDict[genre] = genreDict[genre] + 1
 
+def total_readability(json_array, read_dict):
+    for object in json_array:
+        songs = object["songs"]
+        for song in songs:
+            fog = song["fog_index"]
+            if fog < 3:
+                read_dict["x<3"] = read_dict["x<3"]+1
+            elif fog >= 3 and fog < 5:
+                read_dict["3<x<5"] = read_dict["3<x<5"]+1
+            elif fog >= 5 and fog < 7:
+                read_dict["5<x<7"] = read_dict["5<x<7"]+1
+            else:
+                read_dict[">=7"] = read_dict[">=7"]+1
+
+def total_difficulty(json_array, read_dict):
+    for object in json_array:
+        songs = object["songs"]
+        for song in songs:
+            fog = song["fog_index"]
+            if fog < 25:
+                read_dict["<25"] = read_dict["<25"]+1
+            elif fog >= 3 and fog < 5:
+                read_dict["25>x>50"] = read_dict["25>x>50"]+1
+            elif fog >= 5 and fog < 7:
+                read_dict["50>x>75"] = read_dict["50>x>75"]+1
+            else:
+                read_dict[">75"] = read_dict[">75"]+1
+
 def main():
     filename = "cleandata.json"
 
@@ -27,12 +55,29 @@ def main():
             "blues" : 0,
             "reggae" : 0
     }
-    
+
+    readability = {
+        "x<3" : 0,
+        "3<x<5" : 0,
+        "5<x<7" : 0,
+        ">=7" : 0
+    }
+
+    difficulty = {
+        "<25": 0,
+        "25<x<50" : 0,
+        "50<x<75" : 0,
+        ">75" : 0
+    }
+
+
 
     with open(filename) as json_file:
         data = json.load(json_file)
         total_genre(data, genres)
-        print(genres)
+        total_readability(data, readability)
+        total_difficulty(data, difficulty)
+        print(difficulty)
 
 if __name__ == "__main__":
     main()
